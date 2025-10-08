35a34cf95e8915bed0f57eadac2bdb33281b71e0
Leandro Cruz <leandrocruz2000@gmail.com>
2025-10-08 18:34:41 -0300

    Agrego navegacion BrowserRouter, Route, Routes sobre App. Y sumo js para datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 function App() {
   return (
-    <>
-      <NavBar/>
-      <ItemListContainer greeting='INDUMENTARIA 2025'/>
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<ItemListContainer greeting='INDUMENTARIA 2025' />}></Route>
+        <Route path='/detalle/idParam' element={<ItemListContainer />}></Route>
+        <Route path='/categoria/catParam' element={<ItemListContainer greeting='' />}></Route>
+        <Route path='*' element={<div><h2>Error 404</h2></div>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
