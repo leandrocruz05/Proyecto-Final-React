5d6e71d44579a82487b0657e941518eb061dfd91
Leandro Cruz <leandrocruz2000@gmail.com>
2025-10-13 19:36:59 -0300

    Ultimos cambios

--- src/data/MockAPI.js
+++ src/data/MockAPI.js
@@ -4,20 +4,20 @@ export default function getProductos() {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(productos)
-        }, 5000);
+        }, 0);
     })
 }
 
 export function getProductosById(idParam) {
     return new Promise((resolve) => {
-        const itemRequerido = productos.find((item) => item.id == parseInt(idParam))
-        setTimeout(() => { resolve(itemRequerido) }, 500)
+        const itemRequerido = productos.find((item) => item.id === parseInt(idParam))
+        setTimeout(() => { resolve(itemRequerido) }, 0)
     })
 }
 
 export function getProductosByCateg(catParam) {
     return new Promise((resolve) => {
         const itemRequerido = productos.filter((item) => item.category === catParam)
-        setTimeout(() => { resolve(itemRequerido) }, 500)
+        setTimeout(() => { resolve(itemRequerido) }, 0)
     })
 }
